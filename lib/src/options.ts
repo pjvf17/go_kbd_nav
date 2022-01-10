541da5de34bb129282e660df8d21e45c2c7173da
Pascal von Fintel <pjvf17@gmail.com>
2022-01-10 16:19:04 -0500

    Added duplicate keybind detection

--- lib/src/options.ts
+++ lib/src/options.ts
@@ -3,20 +3,43 @@ import { reset } from "./default";
 
 var Mousetrap = require("mousetrap-record")(require("mousetrap"));
 
-const recordSequence = (
-  e: MouseEvent,
+const recordSequence = async (
   elem: HTMLInputElement,
-  shortcuts: { [index: string]: string },
+  shortcuts: { [index: HTMLInputElement["id"]]: string },
 ) => {
-  console.log("checking");
-  console.log(e);
-  Mousetrap.record(async function (sequence: string[]) {
+  await Mousetrap.record(async function (sequence: string[]) {
     // sequence is an array like ['ctrl+k', 'c']
     elem.value = sequence.join(" ");
     shortcuts[elem.id] = sequence.join(" ");
     await Browser.storage.sync.set({ shortcuts });
+    checkDuplicates(shortcuts);
   });
 };
+
+const checkDuplicates = (shortcuts: { [index: string]: string }) => {
+  let seen: { [index: HTMLInputElement["id"]]: string[] } = {};
+  for (let inputID in shortcuts) {
+    let input = document.getElementById(inputID) as HTMLInputElement;
+    input.style.backgroundColor = "";
+    if (seen[shortcuts[inputID]] !== undefined) {
+      seen[shortcuts[inputID]].push(inputID);
+    } else {
+      seen[shortcuts[inputID]] = [inputID];
+    }
+  }
+  for (let e in seen) {
+    if (seen[e].length > 1) {
+      seen[e].forEach((el) => {
+        let input = document.getElementById(el) as HTMLInputElement;
+        input.style.backgroundColor = "red";
+      });
+    }
+  }
+};
+
+// const consol: HTMLParagraphElement = document.getElementById(
+//   "consol",
+// ) as HTMLParagraphElement;
 
 const main = async () => {
   let inputs = document.querySelectorAll("input");
@@ -29,12 +52,17 @@ const main = async () => {
     ) as HTMLButtonElement;
     button?.addEventListener(
       "click",
-      (e) => recordSequence(e, input, shortcuts),
+      () => recordSequence(input, shortcuts),
     );
   });
+  checkDuplicates(shortcuts);
   let resetButton = document.querySelector("#reset");
   // If reset button is pressed, call reset and then run main again to refresh keys
-  resetButton?.addEventListener("click", () => reset().then(() => main()));
+  resetButton?.addEventListener("click", () =>
+    reset().then(() => {
+      inputs.forEach((input) => input.style.backgroundColor = "");
+      main();
+    }));
 };
 
 document.addEventListener("DOMContentLoaded", main);
